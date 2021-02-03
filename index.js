1a2a0d0d221754e9fefaa5c99cd3904941458956
Erik Zhao <erikzhao98@gmail.com>
2021-02-03 16:04:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+    let array = []
+    for (let i = 0; i  < arr.length; i++) {
+        array.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return array
+}
+
+function countDown(int) {
+    let num = 0
+    while (num <= int) {
+        console.log(int)
+        int--
+    }
+} 
