1153f837d2fabc992abf21ee6a18a181f75b43db
Erik Zhao <erikzhao98@gmail.com>
2021-02-03 16:02:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let array = []
 function writeCards(arr, event) {
+    let array = []
     for (let i = 0; i  < arr.length; i++) {
         array.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
     }
     return array
 }
+
+function countDown(int) {
+    let num = 0
+    while (num <= int) {
+        console.log(int)
+        int--
+    }
+} 
