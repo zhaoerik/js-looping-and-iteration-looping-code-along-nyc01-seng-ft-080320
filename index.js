815856512b30c1c5e04f4fdfdfae2fe6e2e1f5a4
Erik Zhao <erikzhao98@gmail.com>
2021-02-03 15:52:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+// Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push('Odie');
+prepend.unshift('Odie');
+removeLast.pop();
+removeFirst.shift();
+
+
